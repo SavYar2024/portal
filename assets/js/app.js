dd0d821bda9c475420cda0f068239ad764d47bc6
SavYar2024 <156315545+SavYar2024@users.noreply.github.com>
2026-09-16 19:51:48 +0300

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,5 +0,0 @@
-// Демонстраційна версія: дані беруться з assets/js/data.js.
-// У робочій версії тут буде звернення до REST API бекенда.
-document.querySelectorAll('.gallery img').forEach(img=>{
-  img.addEventListener('click',()=>window.open(img.src,'_blank'));
-});
